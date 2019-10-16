f1bbf28c7adcbca7c03d1d29edde47e9c5539d24
grpltda <grpiccoli@gmail.com>
2019-10-16 10:56:37 -0300

    remove git cache

---
+++ ConsultaMD/src/update.sh
@@ -0,0 +1,11 @@
+﻿cd /media/guillermo/WD3DNAND-SSD-1TB/ConsultaMD/
+git pull
+cd ConsultaMD/
+dotnet bundle
+libman restore
+dotnet publish -r linux-x64 -c Release
+systemctl stop kestrel-consultamd.service ;\
+rm -r ../../webapps/consultamd/;\
+mkdir -p ../../webapps/consultamd/;\
+rsync -auv bin/Release/netcoreapp2.2/linux-x64/publish/* ../../webapps/consultamd/; \
+systemctl start kestrel-consultamd.service
